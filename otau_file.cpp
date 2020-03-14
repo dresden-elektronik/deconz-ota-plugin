949b9e31ee871b90f0beba4a01696e4970b69628
ebaauw <erik.baauw@xs4all.nl>
2020-03-14 14:42:19 +0100

    Update otau_file.cpp
    - Refactor logic to handle `.sbl-ota` files.  When segment contains an invalid `length`, the remainder of the file is added as segment.  The bytes at the `tag` and `length` positions are preserved.
    - Don't replace non-prinatble characteris in header `string` value.
    - Debug messages.

--- otau_file.cpp
+++ otau_file.cpp
@@ -4,6 +4,7 @@
 #include "otau_file.h"
 
 #define MANDATORY_HEADER_LENGTH 56
+#define SEGMENT_HEADER_LENGTH 6
 
 /*! The constructor.
  */
@@ -51,8 +52,7 @@ QByteArray OtauFile::toArray()
         for (;it != end; ++it)
         {
             totalImageSize += (2 + 4); // fixed tag size
-            it->length = (uint32_t)it->data.size();
-            totalImageSize += it->length;
+            totalImageSize += (uint32_t)it->data.size();
         }
     }
 
@@ -91,6 +91,9 @@ QByteArray OtauFile::toArray()
         stream << maxHardwareVersion;
     }
 
+    DBG_Printf(DBG_OTA, "OTAU: %s: %d bytes\n", qPrintable(path), totalImageSize);
+    DBG_Printf(DBG_OTA, "OTAU:   ota header (%u bytes)\n", headerLength);
+
     // append tags
     {
         std::list<SubElement>::iterator it = subElements.begin();
@@ -99,12 +102,16 @@ QByteArray OtauFile::toArray()
         {
             stream << it->tag;
             stream << it->length;
-            for (uint i = 0; i < it->length; i++)
+            DBG_Printf(DBG_OTA, "OTAU:   tag 0x%04X, length 0x%08X (%d bytes)\n", it->tag, it->length, it->data.size() + SEGMENT_HEADER_LENGTH);
+
+            for (int i = 0; i < it->data.size(); i++)
             {
                 stream << (uint8_t)it->data[i];
             }
         }
     }
+
+    DBG_Printf(DBG_OTA, "OTAU:   packed %d bytes\n", qPrintable(path), arr.length());
 
     return arr;
 }
@@ -114,8 +121,11 @@ QByteArray OtauFile::toArray()
  */
 bool OtauFile::fromArray(const QByteArray &arr)
 {
+    DBG_Printf(DBG_OTA, "OTAU: %s: %d bytes\n", qPrintable(path), arr.length());
+
     if (arr.size() < MANDATORY_HEADER_LENGTH)
     {
+        DBG_Printf(DBG_OTA, "OTAU: %s: not an ota file (too small)\n", qPrintable(path));
         return false;
     }
 
@@ -124,6 +134,7 @@ bool OtauFile::fromArray(const QByteArray &arr)
     int offset = arr.indexOf(hdr);
     if (offset < 0)
     {
+        DBG_Printf(DBG_OTA, "OTAU: %s: not an ota file (header not found)\n", qPrintable(path));
         return false;
     }
 
@@ -131,11 +142,10 @@ bool OtauFile::fromArray(const QByteArray &arr)
     QDataStream stream(arr);
     stream.setByteOrder(QDataStream::LittleEndian);
 
-    while (offset > 0)
+    for (int i = 0; i < offset; i++)
     {
         quint8 dummy;
         stream >> dummy;
-        offset--;
     }
 
     stream >> upgradeFileId;
@@ -144,6 +154,7 @@ bool OtauFile::fromArray(const QByteArray &arr)
 
     if (headerLength < MANDATORY_HEADER_LENGTH)
     {
+        DBG_Printf(DBG_OTA, "OTAU: %s: not an ota file (invalid header length)\n", qPrintable(path));
         return false;
     }
 
@@ -155,8 +166,6 @@ bool OtauFile::fromArray(const QByteArray &arr)
     for (uint i = 0; i < sizeof(headerString); i++)
     {
         stream >> headerString[i];
-        if (!isprint(headerString[i]))
-            headerString[i] = ' ';
     }
 
     stream >> totalImageSize;
@@ -189,58 +198,59 @@ bool OtauFile::fromArray(const QByteArray &arr)
         processedLength++;
     }
 
+    DBG_Printf(DBG_OTA, "OTAU:   offset %6d: ota header (%u bytes)\n", offset, processedLength);
+
     // read tags
 
     subElements.clear();
 
-    while (!stream.atEnd())
+    while (arr.size() - offset - processedLength >= SEGMENT_HEADER_LENGTH)
     {
         SubElement sub;
+        int size = 0;
+        int start = offset + processedLength;
 
         stream >> sub.tag;
-        if (stream.atEnd())
-        {
-            break; // invalid tag
-        }
+        processedLength += 2;
         stream >> sub.length;
+        processedLength += 4;
 
-        if (sub.length > totalImageSize)
+        if (sub.length > arr.size() - offset - processedLength)
         {
-            if (subElements.empty())
-            {
-//                DBG_Printf(DBG_OTA, "invalid length %u for tag 0x%04X total image size %u (treat hole data section as update)\n", sub.length, sub.tag, totalImageSize);
-//                sub.tag = 0;
-//                sub.length = (arr.size() - offset) - headerLength;
-//                stream.device()->seek(arr.size() - sub.length);
-            }
-            else
-            {
-                DBG_Printf(DBG_OTA, "invalid length %u for tag 0x%04X total image size %u\n", sub.length, sub.tag, totalImageSize);
-                break;
-            }
+            size = arr.size() - offset - processedLength;
+        }
+        else
+        {
+            size = sub.length;
         }
 
         while (!stream.atEnd())
         {
-            if (sub.data.size() == static_cast<int>(sub.length))
+            if (sub.data.size() == size)
             {
                 break;
             }
 
             uint8_t ch;
             stream >> ch;
+            processedLength++;
             sub.data.append(static_cast<char>(ch));
-            Q_ASSERT(sub.data.size() <= static_cast<int>(sub.length));
+            Q_ASSERT(sub.data.size() <= size);
         }
 
-        if (sub.data.size() == static_cast<int>(sub.length))
+        if (sub.data.size() == size)
         {
             subElements.push_back(sub);
+            DBG_Printf(DBG_OTA, "OTAU:   offset %6u: tag 0x%04X, length 0x%08X (%d bytes)\n", start, sub.tag, sub.length, size + SEGMENT_HEADER_LENGTH);
         }
         else
         {
-            DBG_Printf(DBG_OTA, "sub.data.size %d does not match expected size %u\n", sub.data.size(), sub.length);
+            DBG_Printf(DBG_OTA, "OTAU:   offset %6u: ignore tag 0x%04X with invalid length\n", start, sub.tag);
         }
+    }
+    if (!stream.atEnd())
+    {
+        DBG_Printf(DBG_OTA, "OTAU:   offset %6u: ignore trailing %d bytes\n", offset + processedLength, arr.size() - offset - processedLength);
     }
 
     raw = arr;
